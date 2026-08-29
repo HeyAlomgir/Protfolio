13ebac0102244cac3f31fb15065cc6fc831e0357
Alomgir Hossain <alomgirhosssain71@gmail.com>
2026-08-30 01:55:12 +0600

    up:certification section

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -252,7 +252,6 @@ const Navbar = () => {
         { name: 'Skills', id: 'skills' },
         { name: 'Services', id: 'services' },
         { name: 'Education', id: 'education' },
-        { name: "Certifications", id: "certifications"},
         { name: 'Projects', id: 'projects' },
         { name: 'Contact', id: 'contact' },
     ];
